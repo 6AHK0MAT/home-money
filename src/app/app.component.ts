7b386df0e5d6a92d87d4db5de70570e5d8d4f7dd
6AHK0MAT <makhmutov.va@mail.ru>
2018-05-04 13:48:02 +0700

    Часть 2. Практика. Разработка сервиса - Домашняя бухгалтерия Страница логина. Получение данных с сервера

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'wfm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router) {}
-  ngOnInit(){
-    this.router.navigate(['/login']);
-  }
-}
+export class AppComponent {}
