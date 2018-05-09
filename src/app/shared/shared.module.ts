080fcb88d38415b9e283da8a6faf11c731a64479
6AHK0MAT <makhmutov.va@mail.ru>
2018-05-09 16:46:14 +0700

    Часть 2. Практика. Разработка сервиса - Домашняя бухгалтерия Страница 404

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
-  imports: [ReactiveFormsModule, FormsModule, NgxChartsModule],
-  exports: [ReactiveFormsModule, FormsModule, NgxChartsModule]
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    NgxChartsModule
+  ],
+  exports: [
+    ReactiveFormsModule,
+    FormsModule,
+    NgxChartsModule
+  ]
 })
 export class SharedModule {}
